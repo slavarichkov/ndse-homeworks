edf25d92180ea37eaac82e56020f0d6e0ffc20f4
Slava Richkov <v.v.richkov@yandex.ru>
2023-04-06 14:13:41 +0800

    feat: работа с загрузкой и создагнием книги

--- 007-middleware/controllers/books.js
+++ 007-middleware/controllers/books.js
@@ -1,4 +1,8 @@
+const express = require('express');
+
+const fileMulter = require('./../middlewares/file')
 const { books } = require('./../utils/db')
+
 let idBook = 2; // хардкодим айди книги
 let newBooks = books;
 
@@ -18,6 +22,14 @@ const getBookId = (req, res) => { // найти книгу по айди
 
 const createBook = (req, res) => { // создать книгу
     idBook = (idBook + 1);
+    fileMulter.single("name-book"), //Принимает один файл с именем name-book. Файл будет сохранен в req.file.
+        (req, res) => {
+            if (req.file) {
+                const { path } = req.file // полный путь к загружаемому файлу
+                res.json({ path })
+            }
+            res.json()
+        }
     books.push({
         id: idBook.toString(),
         title: "string",
@@ -26,7 +38,7 @@ const createBook = (req, res) => { // создать книгу
         favorite: "string",
         fileCover: "string",
         fileName: "string",
-        fileBook: "string",
+        fileBook: path,
     })
     let newBook = books.find((book) => book.id === idBook.toString())
     res.status(200).send({ newBook });
@@ -48,4 +60,23 @@ const deleteBook = (req, res) => { // удалить книгу
     res.status(200).send({ massage: 'ok' });
 };
 
-module.exports = { getBooks, getBookId, createBook, updateBook, deleteBook };
+const downloadBook = (req, res) => { // скачать книгу
+    const { Id } = req.params; // получим из объекта запроса уникальный id книги
+    let findBook = books.find((book) => book.id === Id) // найдем книгу по id
+    let placeBook = findBook.fileBook; // найдем путь до книги для скачивания
+
+    express.static(__dirname + placeBook)
+}
+
+const uploadBook = (req, res) => { // загрузить книгу
+    fileMulter.single("name-book"), //Принимает один файл с именем name-book. Файл будет сохранен в req.file.
+        (req, res) => {
+            if (req.file) {
+                const { path } = req.file // полный путь к загружаемому файлу
+                res.json({ path })
+            }
+            res.json()
+        }
+}
+
+module.exports = { getBooks, getBookId, createBook, updateBook, deleteBook, downloadBook };
--- 007-middleware/routes/books.js
+++ 007-middleware/routes/books.js
@@ -1,11 +1,11 @@
 const router = require('express').Router(); // создали роутер
-const {getBooks, getBookId, createBook, updateBook, deleteBook} = require('./../controllers/books'); // контроллеры
-const fileMulter = require('./../middlewares/file')
+const {getBooks, getBookId, createBook, updateBook, deleteBook, downloadBook} = require('./../controllers/books'); // контроллеры
 
 router.get('/api/books/', getBooks);
 router.get('/api/books/:id', getBookId);
 router.post('/api/books', createBook)
 router.put('/api/books/:id', updateBook)
 router.delete('/api/books/:id', deleteBook)
+router.get('/api/books/:id/download', downloadBook);
 
 module.exports = router;
