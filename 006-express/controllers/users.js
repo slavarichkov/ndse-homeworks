6c8d162d0964c38d53ff43f824b91ca7652a35f2
Slava Richkov <v.v.richkov@yandex.ru>
2023-04-06 11:14:37 +0800

    fix: контроллер юзера

--- 006-express/controllers/users.js
+++ 006-express/controllers/users.js
@@ -1,7 +1,4 @@
 const login = (req, res) => { // логин
-    const { // получим из объекта запроса имя и описание пользователя
-        email, password,
-    } = req.body;
     res.status(201).send(
         { id: 1, mail: "test@mail.ru" },
     );
