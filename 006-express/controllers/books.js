8aa0ae7db3b463fe85e1ab19aa8160ab41681e75
Slava Richkov <v.v.richkov@yandex.ru>
2023-04-06 11:13:27 +0800

    feat: контроллеры книг

--- 006-express/controllers/books.js
+++ 006-express/controllers/books.js
@@ -1,43 +1,47 @@
-const getBooks = (req, res) => {
-    res.status(201)
-    //работа с БД
+const { books } = require('./../utils/db')
+let idBook = "2"; // хардкодим айди книги
+
+const getBooks = (req, res) => { // получить все книги
+    res.status(200).send({ books });
 }
 
-const getBookId = (req, res) => {
-    const { id } = req.params; // получим из объекта запроса имя и описание пользователя
-    //работа с БД
+const getBookId = (req, res) => { // найти книгу по айди
+    let findBook = books.find((book) => book.id === idBook)
+    if (findBook !== undefined) {
+        res.status(200).send({ findBook });
+    } else {
+        res.status(404).send({ massage: "книга не найдена" });
+    }
 }
 
-const createBook = (req, res) => {
-    const {
-        id,
-        title,
-        description,
-        authors,
-        favorite,
-        fileCover,
-        fileName
-    } = req.body
-    //работа с БД
+const createBook = async (req, res) => { // создать книгу
+    idBook = (idBook + 1).toString();
+    await books.push({
+        id: idBook,
+        title: "string",
+        description: "string",
+        authors: "string",
+        favorite: "string",
+        fileCover: "string",
+        fileName: "string"
+    })
+    let newBook = books.find((book) => book.id === idBook)
+    res.status(200).send({ newBook });
 }
 
 const updateBook = (req, res) => {
-    const {
-        id,
-        title,
-        description,
-        authors,
-        favorite,
-        fileCover,
-        fileName
-    } = req.body
-    const { idBook } = req.params
-    //работа с БД
+    let findBook = books.find((book) => book.id === idBook)
+    if (findBook !== undefined) {
+        res.status(200).send({ findBook });
+    } else {
+        res.status(404).send({ massage: "книга не найдена" });
+    }
 }
 
-const deleteBook = (req, res) => {
-    const { idBook } = req.params
-    //работа с БД
+const deleteBook = async (req, res) => {
+    idBook = (idBook - 1).toString();
+    await books.filter((book) => book.id !== idBook)
+    res.status(200).send({ massage: 'ok' });
 };
 
 module.exports = { getBooks, getBookId, createBook, updateBook, deleteBook };
---
+++ 006-express/utils/db.js
@@ -0,0 +1,22 @@
+const books = [
+    {
+        id: "1",
+        title: "string1",
+        description: "string1",
+        authors: "string1",
+        favorite: "string1",
+        fileCover: "string1",
+        fileName: "string1"
+    },
+    {
+        id: "2",
+        title: "string2",
+        description: "string2",
+        authors: "string2",
+        favorite: "string2",
+        fileCover: "string2",
+        fileName: "string2"
+    }
+];
+
+module.exports = { books }
